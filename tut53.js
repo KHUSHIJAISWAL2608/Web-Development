d278191a8285a3bccaa62713da1a6edd479d960d
Khushi Jaiswal <104351996+KHUSHIJAISWAL2608@users.noreply.github.com>
2023-04-05 00:09:47 +0530

    Add files via upload

---
+++ tut53.js
@@ -0,0 +1,20 @@
+// console.log('this is khushi jaiswal ');
+// let name="khushi jaiswal"
+// console.log(name+"is a good girl");
+// let name1="avinash singh ";
+// console.log(name1+"is my husband");\
+let name1='khushi jaiswal';
+function greet(name1,greetText='Greetings from Javascript'){
+    console.log( greetText +" "+ name);
+    console.log(name1 + " is good boy/girl");
+}
+let greetText = "Good Afternoon";
+greet(name1);
+greet(name1,greetText);
+function sum(a,b,c){
+    d=a+b+c;
+    return d;
+    console.log("khushi");
+}
+let returnVal=sum(1,2,3)
+console.log(returnVal);
