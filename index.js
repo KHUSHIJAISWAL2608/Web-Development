46796d06dd837cdfb8b0e93ca93cc0b9e8119011
Khushi Jaiswal <104351996+KHUSHIJAISWAL2608@users.noreply.github.com>
2023-04-09 00:00:57 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,3 @@
+const average=require("./mod");
+console.log(mod.average([3,4]));
+console.log("this is index.js");
---
+++ mod.js
@@ -0,0 +1,9 @@
+console.log("This is module");
+function average(arr){
+    let sum=0;
+    array.array.forEach(element => {
+          sum+=element;
+    });
+return sum/arr.length;
+}
+module.exports=average;
