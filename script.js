5072ba193f90329a78e27c04fc647051c0624f5e
Khushi Jaiswal <104351996+KHUSHIJAISWAL2608@users.noreply.github.com>
2023-04-11 00:47:50 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,20 @@
+$(document).ready(function() {
+    $("#btn").click(function(e){
+    var jsonData = {};
+    var formData = $("#myform").serializeArray();
+    //console.log(formData);
+    $.each(formData, function() {
+    if (jsonData[this.name]) {
+    if (!jsonData[this.name].push) {
+    jsonData[this.name] = [jsonData[this.name]];
+    }
+    jsonData[this.name].push(this.value || '');
+    } else {
+    jsonData[this.name] = this.value || '';
+    }
+    });
+    console.log(jsonData);
+    e.preventDefault();
+    });
+    });
+    
